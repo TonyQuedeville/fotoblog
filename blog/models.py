38b134b67c18f0bec0a24cd0b072f7f8bde90085
Tony QUEDEVILLE <tonyquedeville@gmail.com>
2022-05-10 21:41:35 +0200

    Correction Erreur photo.save()

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.conf import settings
 class Photo(models.Model):
     image = models.ImageField(verbose_name='image')
     caption = models.CharField(max_length=128, blank=True, verbose_name="légende")
-    upload = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    uploader = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     date_created = models.DateTimeField(auto_now_add=True)
 
 class Blog(models.Model):
